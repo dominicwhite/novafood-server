03fb8e312800cfa0f7a98fb2e71ebaedde5cf063
Dominic White <dewhite4@gmail.com>
2018-12-22 12:56:07 -0500

    API returns location-based restaurant results

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,7 @@
-import random
-import sqlite3
-
-from flask import Flask, g, json
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.event import listens_for
 from sqlalchemy.sql import select, func
-#from flask_migrate import Migrate
 
 from config import Config
 
@@ -19,10 +15,5 @@ def load_spacialite(dbapi_conn, connection_record):
     dbapi_conn.load_extension('/usr/lib/x86_64-linux-gnu/mod_spatialite.so')
     print(db.app)
 db.engine.execute(select([func.InitSpatialMetaData(1)]))
-#migrate = Migrate(app, db)
 
 from app import routes, models
-
-@app.before_first_request
-def init_request():
-    db.create_all()
--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,34 @@
-import random
-from flask import json
-from geoalchemy2 import functions, WKBElement, WKTElement
+from flask import json, request
+from geoalchemy2 import WKTElement
 from sqlalchemy.sql import func
-from geoalchemy2 import Geometry, shape
-
-from shapely.geometry import Point
+from geoalchemy2 import Geometry
 
 from app import app, db
 from app.models import Restaurant
 
-@app.route("/<path:path>")
-def home(path):
+@app.route("/restaurants/")
+def restaurants_view():
     geometry_type = Geometry(management=True, use_st_prefix=False)
-    restaurants = Restaurant.query.filter(
+    lat = request.args.get('lat', default=38.864428, type=float)
+    lon = request.args.get('lon', default=-77.088477, type=float)
+    count = request.args.get('count', default=10, type=int)
+    radius = request.args.get('radius', default=100, type=int)
+    restaurants = db.session.query(Restaurant, func.ST_X(Restaurant.location), func.ST_Y(Restaurant.location)).filter(
         func.PtDistWithin(
             Restaurant.location,
-            WKTElement('POINT(38.864428 -77.088477)', srid=4326),
-            10000000000000,
+            WKTElement(f'POINT({lon} {lat})', srid=4326),
+            radius,
             type_=geometry_type
         )
-    ).all()
+    ).order_by(
+        func.ST_Distance(
+            Restaurant.location,
+            WKTElement(f'POINT({lon} {lat})', srid=4326)
+        )
+    ).limit(count)
     restaurant_json = [{
-        'name': r.restaurant_name,
-        'coords': r.location.data
+        'name': r[0].restaurant_name,
+        'lon': r[1],
+        'lat': r[2]
     } for r in restaurants]
     return json.jsonify(restaurant_json)
