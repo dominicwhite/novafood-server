dc28e802a0e7f2d64c9e5efd8255418f854f0411
Dominic White <dewhite4@gmail.com>
2019-01-17 17:17:01 -0500

    Add additional restaurant filtering

--- app/routes.py
+++ app/routes.py
@@ -6,13 +6,24 @@ from geoalchemy2 import Geometry
 from app import app, db
 from app.models import Restaurant, Inspection
 
-@app.route("/restaurants/")
-def restaurants_view():
+def get_data_from_db(filter_by, lat, lon, count, radius):
+    # TODO: filter by best & worst as well. Requires some measure of "goodness" and its calculation either during the
+    #  query or during the ETL process
     geometry_type = Geometry(management=True, use_st_prefix=False)
-    lat = request.args.get('lat', default=38.864428, type=float)
-    lon = request.args.get('lon', default=-77.088477, type=float)
-    count = request.args.get('count', default=10, type=int)
-    radius = request.args.get('radius', default=100, type=int)
+    if filter_by == "recent":
+        restaurants = db.session.query(Restaurant, func.ST_X(Restaurant.location), func.ST_Y(Restaurant.location), Inspection).filter(
+            func.PtDistWithin(
+                Restaurant.location,
+                WKTElement(f'POINT({lon} {lat})', srid=4326),
+                radius,
+                type_=geometry_type
+            )
+        ).filter(
+            Restaurant.id == Inspection.restaurant_id
+        ).order_by(
+            -Inspection.year, -Inspection.month, -Inspection.day
+        ).limit(count)
+        return restaurants
     restaurants = db.session.query(Restaurant, func.ST_X(Restaurant.location), func.ST_Y(Restaurant.location)).filter(
         func.PtDistWithin(
             Restaurant.location,
@@ -26,12 +37,24 @@ def restaurants_view():
             WKTElement(f'POINT({lon} {lat})', srid=4326)
         )
     ).limit(count)
+    return restaurants
+
+
+@app.route("/restaurants/")
+def restaurants_view():
+    lat = request.args.get('lat', default=38.864428, type=float)
+    lon = request.args.get('lon', default=-77.088477, type=float)
+    count = request.args.get('count', default=10, type=int)
+    radius = request.args.get('radius', default=100, type=int)
+    filter_by = request.args.get('filter_by', default='distance', type=str)
+    restaurants = get_data_from_db(filter_by, lat, lon, count, radius)
     restaurant_json = [{
+        'id': r[0].id,
         'name': r[0].restaurant_name,
-        'lon': r[1],
+        'long': r[1],
         'lat': r[2]
     } for r in restaurants]
-    return json.jsonify(restaurant_json)
+    return json.jsonify({'restaurants': restaurant_json})
 
 @app.route('/restaurants/<int:id>/inspections/')
 def inspections_view(id):
